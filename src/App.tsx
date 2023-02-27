5e90cb6d9dadecc261be4bf40ea278d94bc1bbf4
Maxim <me@alzhanov.ru>
2023-02-27 05:18:06 +0300

    More actions

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import { BubbleSortStarter } from "./visualizers/bubble-sort/start";
 import { globalStore } from "./lib";
 import { BubbleSortRender } from "./visualizers/bubble-sort/render";
 import { useVisualizer } from "./lib/hooks";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const App = () => {
-  const {curState, curEvent, events, start, next} = useVisualizer();
+  const {curState, curEvent, events, currentStep, start, next} = useVisualizer();
   const [eventOverride, setEventOverride] = useState<number | undefined>(undefined);
   const curEventOverride = eventOverride !== undefined ? events[eventOverride] : curEvent;
   const curStateOverride = eventOverride !== undefined ? events[eventOverride].state : curState;
-  const onNextClick = () => {
+  const doNext = () => {
     if (eventOverride !== undefined && eventOverride < events.length - 1) {
       setEventOverride(eventOverride + 1);
     } else {
@@ -17,21 +17,50 @@ const App = () => {
       setEventOverride(undefined);
     }
   };
+  const doPrev = () => {
+    if(eventOverride === undefined) {
+      setEventOverride(currentStep - 2);
+    } else if (eventOverride > 0) {
+      setEventOverride(eventOverride - 1);
+    }
+  }
+  useEffect(() => {
+    // key listeners
+    const keyListener = (e: KeyboardEvent) => {
+      if (e.key === "ArrowRight") {
+        doNext();
+      }
+      if (e.key === "ArrowLeft") {
+        doPrev();
+      }
+    };
+    window.addEventListener("keydown", keyListener);
+    return () => {
+      window.removeEventListener("keydown", keyListener);
+    };
+  }, [eventOverride, currentStep]);
+
   return <div>
     <h1>Visualizer</h1>
+    <h2>Current Step: {currentStep}</h2>
+    <button onClick={doNext}>Next</button>
+    <button onClick={doPrev}>Prev</button>
+    <button onClick={() => setEventOverride(undefined)}>Reset</button>
+    <button onClick={() => setEventOverride(0)}>Beginning</button>
+    <button onClick={() => setEventOverride(events.length - 1)}>End</button>
+    <small>Use arrow keys to navigate</small>
     <BubbleSortStarter doStart={start}/>
     <BubbleSortRender curState={curStateOverride} curEvent={curEventOverride}/>
     <div>
       {events && events.map((x, i) => {
         return <div key={i}>
           {i === eventOverride ? "OVERRIDE" : ""}
-          {i === globalStore.currentStep - 1 ? "CUR" : ""}
+          {i === currentStep - 1 ? "CUR" : ""}
           {JSON.stringify(x)}
-          <button onClick={() => setEventOverride(i)}>Override</button>
+          <button onClick={() => setEventOverride(i)}>Jump</button>
           </div>;
       })}
     </div>
-    <button onClick={onNextClick}>Next</button>
   </div>;
 };
 
